43a20f070559c3ba446dc0f171c87fea32459ee1
BenjaminBush <ben.bush4@gmail.com>
2016-10-09 09:15:26 -0500

    mypongo

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template, request, redirect
+import flask.ext.pymongo import PyMongo
 
 app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = 'prod'
+app.config['MONGO_URI'] = 'mongodb://ec2-52-38-154-245.us-west-2.compute.amazonaws.com:27017/prod'
+
+mongo = PyMongo(app)
 
 @app.route('/')
 def main():
@@ -9,6 +15,12 @@ def main():
 @app.route('/index')
 def index():
   return render_template('index.html')
+
+@app.route('api/speakers')
+def getSpeakers():
+	speakers = mongo.db.speakers
+	result = speakers.find({})
+	return result
 
 if __name__ == '__main__':
   app.run(port=33507)
---
+++ routes/views.py
@@ -0,0 +1,2 @@
+from flasak import render_template, request, session, url_for, redirect, jsonify
+import requests, base64 
